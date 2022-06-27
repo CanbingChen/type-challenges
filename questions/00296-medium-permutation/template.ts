24fbf0beb74536e0df114fdfd3efe26e999d7ef1
陈灿冰 <chencanbing@baicizhan.com>
2022-06-27 17:32:12 +0800

    chore(main): 👌Permutation 添加ts三元解释说明

--- questions/00296-medium-permutation/template.ts
+++ questions/00296-medium-permutation/template.ts
@@ -1,5 +1,6 @@
-type Permutation<T extends keyof any> = [T] extends [never] ?
-    [] :
-    {
+// tip: ts三元判断中如果T类型是never会跳过三元判断
+type Permutation<T extends keyof any> = [T] extends [never]
+  ? []
+  : {
       [TT in T]: [TT, ...Permutation<Exclude<T, TT>>]
     }[T]
